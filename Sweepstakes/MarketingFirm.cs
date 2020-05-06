890b658963f40b52cb179c196ab7b981fcb0a2e4
FlyerB <zarandazchi@gmail.com>
2020-05-06 08:46:03 -0500

    Dependency Injection notes

--- Sweepstakes/MarketingFirm.cs
+++ Sweepstakes/MarketingFirm.cs
@@ -4,6 +4,8 @@ using System.Text;
 
 namespace Sweepstakes
 {
+    //This class is where I used Dependency Injection. The benefit this adds to my application, is that I can pass ISweepstakesManager through as the parameter for the MarketingFirm() which allows either a queue or a stack to be created which is chosen by user input.
+
     class MarketingFirm 
     {
         //create a sweepstakes
